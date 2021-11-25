d9bdabba6f6407bcbd004f632aac66eee3ddce1a
sunqi <sunqi@xiao100.com>
2021-11-25 15:00:20 +0800

    feat: 新增购物车组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./assets/images/logo.svg";
 import robots from "./mockdata/robots.json";
 import Robot from "./components/Robot";
 import styles from "./App.module.css";
+import ShoppingCart from "./components/ShoppingCart";
 
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo" />
+        <h1>React Robots</h1>
+      </div>
+      <ShoppingCart />
       <div className={styles.robotList}>
         {robots.map((r) => (
           <Robot id={r.id} name={r.name} email={r.email}></Robot>
